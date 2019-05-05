1e6add8c1f97b8202a3648f139ffd5af824e0093
Sawyerf <sawyer.flink@protonmail.ch>
2019-05-05 15:29:57 +0200

    askljdkjskljdakljdakljfhkjhdsk jfhalkhdfj lksdjhflkdhlkjfh kldhsflkjh asldfhjk;sdh

--- src/readline/signal.c
+++ src/readline/signal.c
@@ -6,19 +6,34 @@
 /*   By: apeyret <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:37:16 by apeyret           #+#    #+#             */
-/*   Updated: 2019/05/03 09:42:18 by ktlili           ###   ########.fr       */
+/*   Updated: 2019/05/05 15:28:29 by apeyret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "readline.h"
+#include "sig.h"
 
 extern t_rdl g_rdl;
+
+t_signal g_sig[] =\
+{
+	{SIGINT, &nothing},
+	{SIGQUIT, &sigcatch},
+	{SIGSTOP, &sigcatch},
+	{SIGWINCH, &resize},
+	{0, sigcatch}
+};
 
 void	resize(int sig)
 {
 	(void)sig;
 	g_rdl.col = getcolumn();
 	reprint(&g_rdl, g_rdl.curs);
+}
+
+void	sigcatch(int sig)
+{
+	ft_printf("Catch Signal: %d\n", sig);
 }
 
 void	nothing(int sig)
@@ -29,6 +44,24 @@ void	nothing(int sig)
 
 void	setsig(void)
 {
-	signal(SIGWINCH, &resize);
-	signal(SIGINT, &nothing);
+	int		i;
+
+	i = 0;
+	while (g_sig[i].sig)
+	{
+		signal(g_sig[i].sig, g_sig[i].func);
+		i++;
+	}
+}
+
+void	unsetsig(void)
+{
+	int		i;
+
+	i = 0;
+	while (g_sig[i].sig)
+	{
+		signal(g_sig[i].sig, SIG_DFL);
+		i++;
+	}
 }
